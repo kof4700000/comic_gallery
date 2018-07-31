763433d8a98b47861bb44c4abc2b084ea73e046a
kof4700000 <liangmiao530@163.com>
2018-07-31 01:18:01 -0400

    add gallery detail page

--- gallery/urls.py
+++ gallery/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^$', views.GalleryView.as_view(), name = "gallery"),
-    url(r'([0-9]+)$', views.gallery_detail, name = "gallery_detail"),
+    #url(r'(?P<PK>[0-9]+)$', views.gallery_detail, name = "gallery_detail"),
+    url(r'(?P<pk>[0-9]+)/vol_([0-9])+/$', views.image_page, name = "image_page"),
+    url(r'(?P<pk>[0-9]+)/$', views.GalleryDetailView.as_view(), name = "gallery_detail"),
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -5,12 +5,36 @@ from django.shortcuts import render
 from .models import Image, Gallery
 from django.views import generic
 # Create your views here.
-    
-def gallery_detail(request, gallery):
-    imgs = Image.objects.filter(id = gallery)
-    img_src = Image.objects.all()[0].path
-    context = {"img_src":img_src}
-    return render(request, "gallery/gallery_detail.html", context)
+
+def image_page(request,pk):
+    return render(request, "gallery/image.html", context={})
+
+class GalleryDetailView(generic.DetailView):
+    model = Gallery
+    template_name = "gallery/gallery_detail.html"
+    context_object_name = "gallery"
+    '''
+    def get_object:
+        ...
+        #注意在urls.py中，必须配置?P<pk>属性。detailview通过urls.py中的此属性获取对应的queryset
+        #详见generic.DetailView代码get_object
+    '''
+    def get_context_data(self, **kwargs):
+        context = super(GalleryDetailView, self).get_context_data(**kwargs)
+        volume = []
+        gallery = self.object
+        #for each in Image.objects.filter(name = gallery.name).distinct('volume'):
+        #DISTINCT ON fields is not supported by this database backend
+        #Django DOC page 1179:QuerySet API reference - QuerySet API - distinct
+        #for each in Image.objects.filter(name = gallery.name).values_list('volume').distinct():
+        #    volume.append(each)
+        #context['volume'] = timezone.now()
+        SQL = "SELECT * FROM image where name = '" + gallery.name + "' GROUP BY VOLUME"
+        #RAW sql must include id
+        for each in Image.objects.raw(SQL):
+            volume.append(each.volume)
+        context['volume'] = volume
+        return context
 
 class GalleryView(generic.ListView):
     model = Gallery
