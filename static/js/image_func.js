6a3b380662e7de49ad7d3f20491fd6669aaadac7
kof4700000 <liangmiao530@163.com>
2018-08-05 00:01:35 -0400

    add page <select><option> for gallery.html & image.html

--- static/js/image_func.js
+++ static/js/image_func.js
@@ -4,6 +4,10 @@
       src = "/static/" + data
       $("#image").attr('src', src)
     })
+    var max_page = $("#max_page").text()
+    for (var i=2;i<=max_page;i++){
+      $("#page_select").append("<option value='" + i + "'>" + i + "</option>");
+    }
   })
   function next_page(){
     //page_num = parseInt($("#current_page").text())
@@ -19,6 +23,7 @@
       $("#image").attr('src', src)
     })
     $("#current_page").text(next_page_num)
+    $("#page_select").val(next_page_num)
   }
   function pre_page(){
     page_num = Number($("#current_page").text())
@@ -33,4 +38,15 @@
       $("#image").attr('src', src)
     })
     $("#current_page").text(next_page_num)
+    $("#page_select").val(next_page_num)
+  }
+  function select_page(){
+    next_page_num = Number($("#page_select").val())
+    var next_page_url = window.location.href + next_page_num
+    $.get(next_page_url, function(data){
+      src = "/static/" + data
+      $("#image").attr('src', src)
+    })
+    $("#current_page").text(next_page_num)
+    $("#page_select").val(next_page_num)
   }
